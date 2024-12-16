942ad45d9616c5749bdbc0ef9a6f78f040f5d61a
Alexey Zenin <alexthebuildr@gmail.com>
2024-12-16 00:08:23 -0700

    first commit

---
+++ src/SeraphPool.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.26;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Stake } from "./libraries/Stake.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
---
+++ src/libraries/Stake.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+library Stake {
+    error InvalidLockMonths(uint256 lockMonths);
+
+    struct Staker {
+        uint256 staked;
+        uint256 lockedFrom;
+        uint256 lockedUntil;
+        uint256 lockMonths; // 1 to 12
+    }
+
+    uint256 internal constant MIN_LOCK_MONTHS = 1;
+    uint256 internal constant MAX_LOCK_MONTHS = 12;
+    uint256 internal constant MIN_WEIGHT = 100; // 100%
+    uint256 internal constant MAX_WEIGHT = 200; // 200%
+
+    function stakerLockedWeight(uint256 lockMonths) internal pure returns (uint256) {
+        if (lockMonths < MIN_LOCK_MONTHS || lockMonths > MAX_LOCK_MONTHS) {
+            revert InvalidLockMonths(lockMonths);
+        }
+        //check steps division precision
+        return MIN_WEIGHT + (lockMonths - 1) * (MAX_WEIGHT - MIN_WEIGHT) / (MAX_LOCK_MONTHS - MIN_LOCK_MONTHS);
+    }
+
+    function getDistribution(Staker memory staker) internal view returns (uint256) {
+        if (staker.lockMonths < MIN_LOCK_MONTHS || staker.lockMonths > MAX_LOCK_MONTHS) {
+            revert InvalidLockMonths(staker.lockMonths);
+        }
+        uint256 weightMultiplier = stakerLockedWeight(staker.lockMonths);
+        uint256 currentTime = block.timestamp;
+        // need to restake or getting 0?
+        uint256 lockDurationLeft = staker.lockedUntil > currentTime ? staker.lockedUntil - currentTime : 0;
+        uint256 totalLockDuration = staker.lockedUntil - staker.lockedFrom;
+        // test values
+        uint256 lockProgressDivisor = MIN_WEIGHT - (lockDurationLeft / totalLockDuration) * MIN_WEIGHT;
+        uint256 distribution = staker.staked * weightMultiplier * lockProgressDivisor / MIN_WEIGHT / MIN_WEIGHT;
+        return distribution;
+    }
+}

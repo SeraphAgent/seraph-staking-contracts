20610e853b71640b44be4cdde5baea6a13d17a93
Alexey Zenin <alexthebuildr@gmail.com>
2025-01-07 19:06:43 -0700

    chore: deploy

---
+++ script/SeraphPool.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import { Script, console } from "forge-std/Script.sol";
+import { SeraphPool } from "../src/SeraphPool.sol";
+
+contract SeraphPoolScript is Script {
+    function setUp() public { }
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        new SeraphPool(0x4f81837C2f4A189A0B69370027cc2627d93785B4, 0);
+        vm.stopBroadcast();
+    }
+}
